3caf972e9fe70345c77ae2371154d1b44f8ca21d
rachelncirl <161595362+rachelncirl@users.noreply.github.com>
2025-04-06 10:01:51 +0100

    Add Discovery of Cart Service
    Add Discovery of Cart Service

---
+++ src/client/cartClient.js
@@ -0,0 +1,103 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+// Load the proto files
+const cart = protoLoader.loadSync('../protos/cart.proto', {});
+const cartProto = grpc.loadPackageDefinition(cart).cart;
+const cartClient = new cartProto.CartService('localhost:50052', grpc.credentials.createInsecure());
+
+function addToCart(req, callback) {
+
+    const id = req.query.id;
+    const userId = req.query.userId;
+
+    let cart = [];
+
+    console.log("Product ID requested: " + id + " for User: " + userId);
+    const call = cartClient.AddToCart({ id, userId });
+
+    call.on("data", (shoe) => {
+        cart.push({
+            id: shoe.id,
+            brand: shoe.brand,
+            price: shoe.price
+        });
+    });
+
+    call.on("end", () => {
+        callback(null, cart);
+    });
+
+    call.on("error", (error) => {
+        console.error(error);
+        callback(error, 'Error occurred while adding to cart');
+    });
+}
+
+function removeFromCart(req, callback) {
+    const id = req.query.id;
+    const userId = req.query.userId;
+
+    let cart = [];
+
+    console.log("Remove Product ID: " + id + " for User: " + userId);
+    const call = cartClient.RemoveFromCart({ id, userId });
+
+    call.on("data", (shoe) => {
+        cart.push({
+            id: shoe.id,
+            brand: shoe.brand,
+            price: shoe.price
+        });
+    });
+
+    call.on("end", () => {
+        callback(null, cart);
+    });
+
+    call.on("error", (error) => {
+        console.error(error);
+        callback(error, 'Error occurred while removing from the cart');
+    });
+}
+
+function refreshCart(req, callback) {
+    const userId = req.query.userId;
+
+    let cart = [];
+
+    console.log("Refresh Cart for User: " + userId);
+    const call = cartClient.GetCartContents({ userId });
+
+    call.on("data", (shoe) => {
+        cart.push({
+            id: shoe.id,
+            brand: shoe.brand,
+            price: shoe.price
+        });
+    });
+
+    call.on("end", () => {
+        callback(null, cart);
+    });
+
+    call.on("error", (error) => {
+        console.error(error);
+        callback(error, 'Error occurred while refreshing cart');
+    });
+}
+
+function clearCart(req, callback) {
+    const userId = req.query.userId;
+
+    cartClient.EmptyCart({ userId }, (error, response) => {
+        if (error) {
+            console.error('Error:', error);
+            callback(error, 'Error occurred while clearing the cart');
+        } else {
+            callback(null, null);
+        }
+    });
+}
+
+module.exports = { addToCart, removeFromCart, refreshCart, clearCart };
--- src/client/gui.js
+++ src/client/gui.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const product = require('./productClient');
+const cart = require('./cartClient');
 const discovery = require('./discoveryClient');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 app.get('/list', (req, res) => {
@@ -29,6 +29,102 @@ app.get('/list', (req, res) => {
                 return;
             }
             res.send(product);
+        });
+    });
+});
+
+app.get('/addToCart', (req, res) => {
+
+    // Discover product service and then fetch product info
+    discovery.discoverService('cartService', (cartService) => {
+        if (!cartService) {
+            res.status(500).send("Service not found");
+            return;
+        }
+
+        cart.addToCart(req, (err, cartItems) => {
+            if (err) {
+                res.status(500).send("Error adding item to cart");
+                return;
+            }
+
+            if (!cartItems) {
+                res.send("Cart not found");
+                return;
+            }
+            res.send(cartItems);
+        });
+    });
+});
+
+app.get('/removeFromCart', (req, res) => {
+
+    // Discover product service and then fetch product info
+    discovery.discoverService('cartService', (cartService) => {
+        if (!cartService) {
+            res.status(500).send("Service not found");
+            return;
+        }
+
+        cart.removeFromCart(req, (err, cartItems) => {
+            if (err) {
+                res.status(500).send("Error removing item to cart");
+                return;
+            }
+
+            if (!cartItems) {
+                res.send("Cart not found");
+                return;
+            }
+            res.send(cartItems);
+        });
+    });
+});
+
+app.get('/refreshCart', (req, res) => {
+
+    // Discover product service and then fetch product info
+    discovery.discoverService('cartService', (cartService) => {
+        if (!cartService) {
+            res.status(500).send("Service not found");
+            return;
+        }
+
+        cart.refreshCart(req, (err, cartItems) => {
+            if (err) {
+                res.status(500).send("Error refreshing the cart");
+                return;
+            }
+
+            if (!cartItems) {
+                res.send("Cart not found");
+                return;
+            }
+            res.send(cartItems);
+        });
+    });
+});
+
+app.get('/clearCart', (req, res) => {
+
+    // Discover product service and then fetch product info
+    discovery.discoverService('cartService', (cartService) => {
+        if (!cartService) {
+            res.status(500).send("Service not found");
+            return;
+        }
+
+        cart.clearCart(req, (err, cartItems) => {
+            if (err) {
+                res.status(500).send("Error clearing the cart");
+                return;
+            }
+
+            if (!cartItems) {
+                res.send("Cart not found");
+                return;
+            }
+            res.send(cartItems);
         });
     });
 });
